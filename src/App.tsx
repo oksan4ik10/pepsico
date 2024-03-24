1a259157bc172d874ddd411748f438535c601ca4
Kalinina Oksana <ksan4ik92@gmail.com>
2024-03-24 15:13:35 +0300

    feat: add design screen5

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,39 @@
 
 import './App.css'
 
+import urlHR from "./assets/hr-question.png"
+import urlArrow from "./assets/arrow.svg"
+
 function App() {
 
 
   return (
     <>
+      <div className='container'>
+        <div className="wrapper">
+          <div className="points">0/9</div>
+          <div className="task">
+            <div className="character"><img src={urlHR} alt="HR" /></div>
+            <div className="question"><p className="text">Как бы ты разрешил(а)<br />конфликт в команде,<br />где ты лидер?</p></div>
+            <div className="answers">
+              <div className="answer answer__left"><p className="answer__text">Брал(а) все под контроль и ставил(а) строгие рамки – как капитан корабля!</p></div>
+              <div className="answer answer__right"><p className="answer__text">Делали проект по учебе и не уложились в сроки из-за ошибок в планировании</p></div>
+            </div>
+            <div className="arrows">
+              < img className="arrow arrow__left" src={urlArrow} alt="arrowLeft" />
+              <img className="arrow arrow__right" src={urlArrow} alt="arrowRight" />
+
+
+            </div>
+          </div>
+
+
+
+
+
+        </div>
+
+      </div>
 
     </>
   )
