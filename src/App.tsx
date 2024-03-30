1d2e001ba219ff4aaac2129c01a7a351655e09d5
Kalinina Oksana <ksan4ik92@gmail.com>
2024-03-30 19:10:32 +0300

    feat: all project

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import Dialog from './components/Dialog/Dialog';
 
 import './App.css'
 
+import Screen0 from './screens/Screen0/Screen0';
 import Screen2 from './screens/Screen2/Screen2';
 import Task1 from './screens/Task1';
 import Task2 from './screens/Task2/Task2';
+import ScreenLast from './screens/ScreenLast/ScreenLast';
 
 
 
@@ -25,8 +27,7 @@ function App() {
   return (
     <>
       <div className="container">
-
-
+        {screen === 0 && <Screen0 changeScreen={() => changeScreen(1)} />}
         {screen === 1 && <Screen2 changeScreen={() => changeScreen(2)}></Screen2>}
 
         {screen === 2 && <Dialog dialogNum={0} changeScreen={() => changeScreen(3)} />}
@@ -35,12 +36,9 @@ function App() {
         {screen === 4 && <Task1 changeScreen={() => changeScreen(5)} />}
         {screen === 5 && <Dialog dialogNum={2} changeScreen={() => changeScreen(6)} />}
         {screen === 6 && <Task2 changeScreen={() => changeScreen(7)}></Task2>}
+        {screen === 7 && <ScreenLast></ScreenLast>}
 
-
-        <button onClick={() => changeScreen(1)}>Начать</button>
-
-
-      </div>
+      </div >
 
     </>
   )
